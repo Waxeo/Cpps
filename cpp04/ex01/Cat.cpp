d5e7ed998ee13d96f81f004b21b3e3c318aae5d6
Waxeo <matteo.gauvrit@gmail.com>
2023-09-23 11:41:26 +0200

    cp04ex00 fini

---
+++ cpp04/ex01/Cat.cpp
@@ -0,0 +1,35 @@
+#include "Cat.Class.hpp"
+
+Cat::Cat( void ) : Animal()
+{
+	std::cout << "default cat constructor called" << std::endl;
+
+	return ;
+}
+
+Cat::Cat( std::string name ) : Animal(name)
+{
+	std::cout << "Cat constructor called" << std::endl;
+
+	return ;
+}
+
+Cat::~Cat( void )
+{
+	std::cout << "Cat destructor called" << std::endl;
+	return ;
+}
+
+Cat	&Cat::operator=( Cat const &src )
+{
+	this->_Type = src._Type;
+	
+	return *this;
+}
+
+void	Cat::makeSound( void ) const
+{
+	std::cout << "MiaouMiaou" << std::endl;
+
+	return ;
+}
---
+++ cpp04/ex01/Dog.cpp
@@ -0,0 +1,36 @@
+#include "Animal.Class.hpp"
+#include "Dog.Class.hpp"
+
+Dog::Dog( void ) : Animal()
+{
+	std::cout << "default dog constructor called" << std::endl;
+
+	return ;
+}
+
+Dog::Dog( std::string type ) : Animal(type)
+{
+	std::cout << "Dog constructor called" << std::endl;
+
+	return ;
+}
+
+Dog::~Dog( void )
+{
+	std::cout << "Dog destructor called" << std::endl;
+	return ;
+}
+
+Dog	&Dog::operator=( Dog const &src )
+{
+	this->_Type = src._Type;
+	
+	return *this;
+}
+
+void Dog::makeSound( void ) const
+{
+	std::cout << "WafWaf" << std::endl;
+
+	return ;
+}
